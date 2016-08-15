86dedd6927e663a1bd2cdd6e9b8308a1e76daa30
MerryMage <MerryMage@users.noreply.github.com>
2016-08-15 22:57:07 +0100

    revert stupid main.cpp edits

--- source/main.cpp
+++ source/main.cpp
@@ -7,38 +7,32 @@
 #include "tests/kernel/kernel.h"
 #include "tests/gpu/gpu.h"
 
+static unsigned int test_counter = 0;
+static TestCaller tests[] = {
+    FS::TestAll,
+    CPU::Integer::TestAll,
+    CPU::Memory::TestAll,
+    Kernel::TestAll,
+    GPU::TestAll
+};
+
 int main(int argc, char** argv)
 {
-
     gfxInitDefault();
     InitOutput();
 
     consoleClear();
-
     Print("Press A to begin...\n");
-
-    // unsigned int test_counter = 0; // Moving it here causes this app to refuse to boot at all.
 
     while (aptMainLoop()) {
         gfxFlushBuffers();
         gfxSwapBuffers();
 
         hidScanInput();
-        u32 kDown = hidKeysDown();
-        if (kDown & KEY_START) {
+        if (hidKeysDown() & KEY_START) {
             break;
-        } else if (kDown & KEY_A) {
+        } else if (hidKeysDown() & KEY_A) {
             consoleClear();
-
-            unsigned int test_counter = 0; // This configuration works, but obviously only test 0 would run.
-
-            TestCaller tests[] = {
-                FS::TestAll,
-                CPU::Integer::TestAll,
-                CPU::Memory::TestAll,
-                Kernel::TestAll,
-                GPU::TestAll
-            };
 
             if (test_counter < (sizeof(tests) / sizeof(tests[0]))) {
                 tests[test_counter]();
